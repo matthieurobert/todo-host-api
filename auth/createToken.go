a324ad4a873a87f6d7ef299cf9a44eade73eafac
matthieurobert <matthieurobert82@gmail.com>
2021-07-14 21:55:12 +0200

    feat: Change New default user parameters
    Change New default user parameters in create Token request to match with
    actual user informations

--- auth/createToken.go
+++ auth/createToken.go
@@ -6,12 +6,15 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/shaj13/go-guardian/v2/auth"
+	"github.com/todo-host/todo-host-api/entity"
 )
 
 func CreateToken(w http.ResponseWriter, r *http.Request) {
 	token := uuid.New().String()
-	user := auth.NewDefaultUser("admin", "1", nil, nil)
-	auth.Append(TokenStrategy, token, user)
+	_, userInfo, _ := Strategy.AuthenticateRequest(r)
+	user, _ := entity.GetUserByUsername(userInfo.GetUserName())
+	defaultUser := auth.NewDefaultUser(user.Username, string(user.Id), nil, nil)
+	auth.Append(TokenStrategy, token, defaultUser)
 	body := fmt.Sprintf("token: %s \n", token)
 	w.Write([]byte(body))
 }
