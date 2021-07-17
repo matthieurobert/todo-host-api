c808e8db0b91ac8c437cf4a6835be4822da507e8
matthieurobert <matthieurobert82@gmail.com>
2021-07-17 22:15:55 +0200

    feat: Add new handlers to router

--- main.go
+++ main.go
@@ -20,7 +20,12 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/v1/auth/token", middleware.AuthMiddleware(http.HandlerFunc(auth.CreateToken))).Methods("GET")
-	router.HandleFunc("/v1/task/new", middleware.AuthMiddleware(http.HandlerFunc(handler.CreateTaskHandler))).Methods("POST")
+
+	router.HandleFunc("/v1/task", middleware.AuthMiddleware(http.HandlerFunc(handler.CreateTaskHandler))).Methods("POST")
+	router.HandleFunc("/v1/tasks", middleware.AuthMiddleware(http.HandlerFunc(handler.GetTasksHandler))).Methods("GET")
+	router.HandleFunc("/v1/task/{id}", middleware.AuthMiddleware(http.HandlerFunc(handler.GetTaskByIdHandler))).Methods("GET")
+	router.HandleFunc("/v1/task/{id}", middleware.AuthMiddleware(http.HandlerFunc(handler.DeleteTaskByIdHandler))).Methods("DELETE")
+
 	router.HandleFunc("/v1/register", handler.Register).Methods("POST")
 
 	http.ListenAndServe(":8000", router)
