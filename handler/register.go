c823faaa4a734c95c80e5f2540107f10af889128
matthieurobert <matthieurobert82@gmail.com>
2021-08-02 21:33:46 +0200

    feat: Modify Register handler and create it doc

--- handler/register.go
+++ handler/register.go
@@ -2,25 +2,61 @@ package handler
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/todo-host/todo-host-api/entity"
 )
 
+// Register godoc
+// @Summary Register a new user
+// @Description Create a new user with the input payload
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param user body entity.User true "Create User"
+// @Success 200 {object} entity.User
+// @Router /register [post]
 func Register(w http.ResponseWriter, r *http.Request) {
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-	email := r.FormValue("email")
+	// username := r.FormValue("username")
+	// password := r.FormValue("password")
+	// email := r.FormValue("email")
 
-	if username == "" || password == "" || email == "" {
-		http.Error(w, "missed data", http.StatusInternalServerError)
-	}
+	// if username == "" || password == "" || email == "" {
+	// 	http.Error(w, "missed data", http.StatusInternalServerError)
+	// }
 
-	id, err := entity.PostUser(username, password, email)
+	// id, err := entity.PostUser(username, password, email)
+
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+
+	// 	json.NewEncoder(w).Encode(id)
+	// }
+
+	body, err := ioutil.ReadAll(r.Body)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-		json.NewEncoder(w).Encode(id)
 	}
+
+	var user entity.User
+
+	err = json.Unmarshal(body, &user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	if user.Email == "" || user.Username == "" || user.Password == "" {
+		http.Error(w, "missed data", http.StatusInternalServerError)
+	}
+
+	id, err := entity.PostUser(user.Username, user.Password, user.Email)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	json.NewEncoder(w).Encode(id)
 }
